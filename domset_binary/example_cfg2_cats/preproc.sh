54c5999063b117bde64efc7271c815afbaa8d7e8
pedrograz <pedrograz>
2018-05-16 09:17:51 +0200

    ex cfg files for deployment

---
+++ domset_binary/example_cfg2_cats/preproc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mv bee-arena/* ./
+rm -rf bee-arena
+for f in casu-0*; do cd "$f"; rm *domset.csv; mv 20* ../; cd ..; rm -rf "$f"; done
+rename 's/201.*-casu/casu/' 2018*
+for c in casu-0* ; do sed -i '1s/1.0;1.0;1.0;1.0;1.0;1.0/1.0;1.0;1.0;1.0;1.0;1.0;0.0;0.0/' "$c"; done
+sed -i '$d' casu-0*
+
