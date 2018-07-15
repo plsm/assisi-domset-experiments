57f7eca71465215631d70a3daa0bda6d89014194
Pedro Lopes da Silva Mariano <plmariano@ciencias.ulisboa.pt>
2018-07-15 12:08:31 +0100

    Airflow test 4 analysis shows bee where abouts.

--- analysis/plot_airflow_test_4.py
+++ analysis/plot_airflow_test_4.py
@@ -5,6 +5,7 @@ import matplotlib
 matplotlib.use ("Agg")
 
 import argparse
+import csv
 import datetime
 import matplotlib.pyplot
 import numpy
@@ -13,20 +14,37 @@ import yaml
 
 import assisipy.casu
 
+import bee_where_abouts
 import casu_log
 import casu_domset_log
+import util.math
 
 def main ():
     args = process_arguments ()
-    if not args.update_bee_where_abouts_plot:
+    filename = 'bee-where-about_L={}_D={}.csv'.format (args.sampling_length, args.sampling_delta)
+    if not args.update_bee_where_abouts_plot or not os.path.exists (filename):
+        fdw = open (filename, 'w')
+        bee_where_about_writer = csv.writer (fdw, delimiter = ';', quoting = csv.QUOTE_NONNUMERIC)
         for run_number in args.run:
             process_run (
                 run_number = run_number,
                 base_path = os.path.join (args.base_path, args.run_folder_template.format (run_number)),
                 config_filename = args.config,
+                moving_average_length = args.moving_average_length,
+                sampling_length = args.sampling_length,
+                sampling_delta = args.sampling_delta,
+                bee_where_about_writer = bee_where_about_writer,
             )
+        fdw.close ()
+    bee_where_abouts.plot (
+        filename,
+        sampling_length = args.sampling_length,
+        sampling_delta = args.sampling_delta,
+    )
 
-def process_run (run_number, base_path, config_filename):
+def process_run (run_number, base_path, config_filename,
+        moving_average_length,
+        sampling_length, sampling_delta, bee_where_about_writer):
     fn = os.path.join (base_path, config_filename)
     with open (fn) as fd:
         config_data = yaml.safe_load (fd)
@@ -39,12 +57,18 @@ def process_run (run_number, base_path, config_filename):
             first_period_length = config_data ['parameters'] ['first_period_length'],
             airflow_period_length = config_data ['parameters'] ['airflow_duration'],
             third_period_length = config_data ['parameters'] ['third_period_length'],
+            moving_average_length = moving_average_length,
+            sampling_length = sampling_length,
+            sampling_delta = sampling_delta,
+            bee_where_about_writer = bee_where_about_writer,
         )
 
 def process_arena (
         run_number, base_path,
         core_casu_number, leaf_casu_number,
-        first_period_length, airflow_period_length, third_period_length):
+        first_period_length, airflow_period_length, third_period_length,
+        moving_average_length,
+        sampling_length, sampling_delta, bee_where_about_writer):
     # read logs
     casu_logs = {
         a_casu_number : casu_log.CASU_Log (a_casu_number, os.path.join (base_path, 'data_airflow-test/beearena/'))
@@ -54,12 +78,30 @@ def process_arena (
         a_casu_number : casu_domset_log.CASU_DOMSET_Log (a_casu_number, os.path.join (base_path, 'data_airflow-test/beearena/'))
         for a_casu_number in [core_casu_number, leaf_casu_number]
     }
+    # compute activity (needed by bee where about)
+    for a_casu_log in casu_logs.values ():
+        a_casu_log.compute_activity (
+            start_index = 0,
+            end_index = 50,
+            offset = 500,
+            moving_average_length = moving_average_length
+        )
     # create plot
     plot_arena (
         run_number, core_casu_number, leaf_casu_number,
         first_period_length, airflow_period_length, third_period_length,
         casu_logs, casu_domset_logs
-        )
+    )
+    #
+    compute_bee_where_about (
+        run_number,
+        casu_logs,
+        first_period_length,
+        airflow_period_length,
+        third_period_length,
+        sampling_length = sampling_length,
+        sampling_delta = sampling_delta,
+        writer = bee_where_about_writer)
 
 def plot_arena (run_number, core_casu_number, leaf_casu_number,
         first_period_length, airflow_period_length, third_period_length, casu_logs, casu_domset_logs):
@@ -72,9 +114,12 @@ def plot_arena (run_number, core_casu_number, leaf_casu_number,
         ncols = 2,
         gridspec_kw = {
             'width_ratios' : [2, 2],
-#            'wspace' : 0.25
+            'left' : 0.05,
+            'right' : 0.99,
+#            'wspace' : 0.1
         },
-        sharex = True,
+        sharex = 'col',
+        sharey = 'row',
     )
     for axa in axes [0,:]:
         axa.set_ylim (25.15, 37.15)
@@ -110,19 +155,61 @@ def plot_arena (run_number, core_casu_number, leaf_casu_number,
     zero_time = numpy.mean ([a_casu_log.led_actuator [2,0] for a_casu_log in casu_logs.values ()])
     ts = [t + zero_time for t in range (0, first_period_length + airflow_period_length + third_period_length, 60)]
     for axa in [axes [2,0], axes [2,1]]:
+        axa.set_xlabel ('time (mm:ss)', fontsize = 7)
         axa.set_xticks (ts)
         axa.set_xticklabels ([datetime.datetime.fromtimestamp (t - ts [0]).strftime ('%M:%S') for t in ts])
         for ata in [axa.xaxis, axa.yaxis]:
             for tick in ata.get_major_ticks ():
                 tick.label.set_fontsize (7)
-    for idx in [0,1]:
-      for axa in axes[:,idx]:
+    for axa in [axes [i,j] for i in range (3) for j in range (2)]:
+        axa.legend (fontsize = 7)
         for ata in [axa.xaxis, axa.yaxis]:
             for tick in ata.get_major_ticks ():
                 tick.label.set_fontsize (7)
     figure.suptitle ('airflow test 4\nrun #{} core casu {} leaf casu {}'.format (run_number, core_casu_number, leaf_casu_number), fontsize = 9)
     figure.savefig ('casu-log_R{}-C{}-L{}.png'.format (run_number, core_casu_number, leaf_casu_number))
     matplotlib.pyplot.close (figure)
+
+def compute_bee_where_about (run_number, casu_logs, first_period_length, airflow_period_length, third_period_length, sampling_length, sampling_delta, writer):
+    """
+    Compute where the majority of bees are based on sensor activity
+    :param sampling_delta:
+    :type casu_logs: list(casu_log.CASU_Log)
+    """
+    first_period_start_time = numpy.mean ([a_casu_log.led_actuator [2, 0] for a_casu_log in casu_logs.values ()])
+    second_period_start_time = numpy.mean ([a_casu_log.led_actuator [4, 0] for a_casu_log in casu_logs.values ()])
+    third_period_start_time = second_period_start_time + airflow_period_length
+
+    sums_activity = {}
+    for a_start_time, a_length in zip ([first_period_start_time, third_period_start_time], [first_period_length, third_period_length]):
+        sampling_times = [a_start_time + a_length - sampling_delta * ith for ith in range (int (sampling_length / sampling_delta) + 1)]
+        sums_activity [a_start_time] = {}
+        for index, a_casu_log in enumerate (casu_logs.values ()):
+            indexes = [util.math.find_nearest_index (a_casu_log.infrared_raw [:,0], a_time) for a_time in sampling_times]
+            print ('[I] Computing for run {} casus {} and {} bee where about between relative timestamps {:.2f}% and {:.2f}% or indexes {} and {}'.format (
+                run_number,
+                casu_logs.values () [0].number,
+                casu_logs.values () [1].number,
+                100 * (min (sampling_times) - a_casu_log.infrared_raw [:, 0].min ()) / (
+                    a_casu_log.infrared_raw [:, 0].max () - a_casu_log.infrared_raw [:, 0].min ()),
+                100 * (max (sampling_times) - a_casu_log.infrared_raw [:, 0].min ()) / (
+                            a_casu_log.infrared_raw [:, 0].max () - a_casu_log.infrared_raw [:, 0].min ()),
+                min (indexes),
+                max (indexes)
+            ))
+            data = [a_casu_log.moving_average_hits [ith] for ith in indexes]
+            sums_activity [a_start_time][index] = sum (data) / len (data)
+    row = [run_number]
+    row.extend ([a_casu_log.number for a_casu_log in casu_logs.values ()])
+    row.extend ([
+        sums_activity [at][ai]
+        for at in sums_activity.keys ()
+        for ai in sums_activity [at].keys ()])
+    row.extend ([
+        sums_activity [at][1] - sums_activity [at][0]
+        for at in sums_activity.keys ()
+    ])
+    writer.writerow (row)
 
 def process_arguments ():
     parser = argparse.ArgumentParser (
@@ -166,7 +253,7 @@ def process_arguments ():
         default = DEFAULT_MOVING_AVERAGE_LENGTH,
         help = 'Moving average length used when computing the moving average of infrared sensor hits.  Default value is {} seconds.'.format (DEFAULT_MOVING_AVERAGE_LENGTH)
     )
-    DEFAULT_SAMPLING_LENGTH = 1
+    DEFAULT_SAMPLING_LENGTH = 60
     parser.add_argument (
         '--sampling-length',
         metavar = 'L',
